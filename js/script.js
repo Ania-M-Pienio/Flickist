5512dea9159b37a35fa6237618f1298a31f6a6cf
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-15 12:03:31 -0500

    displays and callbacks

--- js/script.js
+++ js/script.js
@@ -2,48 +2,71 @@ const app = {};
 
 app.recentAmount = 3; // app setting for how many recent will display on load
 app.popularAmount = 4; // app setting for how many popular will display on load
+app.resultsAmount = 20;
+app.listCap = 20;
 
 app.list = []; // stores the media added to the list
 
-app.getItemCardHtml = function(itemInfo) {
-  /* receives itemInfo and constructs html */
+app.getItemCardHtml = function(item) {
+  /* receives item and constructs html */
   /* returns the constructed html */
-}
+};
 
-app.displayMedia = function(medias, location) {
-  /* receives medias and location */
+app.getListItemtHtml = function(item) {
+  /* recieves item title and constructs html */
+  /* returns the constructed html */
+};
+
+app.displayMedia = function(medias, location, getHtml) {
+  /* receives medias items, location and htmlconstructor function */
   /* calls forEach on the medias to construct html */
-        /* inside forEach,  calls app.getItemCardHtml and passes the individual */
-  /* apends to display at Recently Added location */
-}
+  /* inside forEach,  calls the given getHtml and passes the individual media item */
+  /* apends to display at given location */
+};
 
 app.getPopular = function() {
   /* makes an ajax call to API */
   /* retrieves popular media */
   /* passes medias and location of Popular Now to displayMedia */
-      /* -----> the length of the array passed is determined by app.popularAmount */
-}
+  /* -----> the length of the array passed is determined by app.popularAmount */
+};
 
-
-app.getRecent = function () {
+app.getRecent = function() {
   /* checks if list has any medias in it */
   /* if it does: */
     /* retrieves the latest most recent added from app.list */
-    /* passes the latest medias to displayMedia and location of the Recently Added */
-    /* ----> the length of the array passed is determined by app.recentAmount */
-}
+    /* passes the retrieved medias to displayMedia with location of the Recently Added, and the getItemCardHtml function (as the getHtml callback) */
+    /* ----> the length of the array passed is determined by app.recentAmount */  
+};
 
 app.getByKeyword = function(keyword) {
   /* receives keyword provided by user */
-  
+  /* makes an AJAX call based on the keyword */
+  /* passes the returned medias to displayMedia along with the location of Results, and the getItemCardHtml function (as the getHtml callback) */
+  /* ---> the length of the array passed is determined by app.resultsAmount */
+};
+
+app.getDetailById = function(id, type) {
+  /* receives the id and type of the media */
+  /* make an appropriate AJAX call */
+  /*  ---> end point is specified by type */
+};
+
+app.addToList = function(media) {
+  /* receives a media */
+  /* checks if there is space in the app.list array */
+  /* if there is: */
+    /* pushes the media to the app.list array */
+    /* passes the app.list to displayMedia along with the location of Watch List, and the getListItemHtml funtion (as the getHtml callback)  */
+  /* if there is not: */
+    /* warning full */
 }
 
-// get medi by keyword
-
-// get media detail
 
 app.init = function() {
-  console.log(`app started`);
+  /* calls getPopular */
+  /* calls getRecent */
+  /* calls Handlers  */
 };
 
 $(() => {
