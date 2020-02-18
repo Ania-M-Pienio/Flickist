2bc7520058e0c9fbb364722caaaf02c6443a8d7f
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-18 15:21:22 -0500

    examples

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ app.api.baseUrl = `https://api.themoviedb.org/3`;
 app.api.key = `9b08417459f02bab4f2533c48a22feab`;
 app.api.lang = `en-US`;
 //-------- SETTINGS ------------------------------------------------------//
-app.recentAmount = 3; // app setting for how many recent will display on load
-app.popularAmount = 3; // app setting for how many popular will display on load
+app.recentAmount = 5; // app setting for how many recent will display on load
+app.popularAmount = 5; // app setting for how many popular will display on load
 app.resultsAmount = 20; // app setting for how many results to show upon search
 app.listAmount = 20; // app setting for max amount of medias that can be be stored in the list at any given time
 // ------- DOM ---------------------------------------------------------- //
 app.dom = {};
 app.dom.$popular = {
   tv: $(`.tv`),
-  movie: $(`.movie`),
+  movie: $(`.movie`)
 };
 app.dom.$recent = $(`.recent`);
 app.dom.$result = $(`.result`);
@@ -58,7 +58,11 @@ app.getItemDetailCard = function(item) {
 /* ------                       DISPLAYS                            -----*/
 /* ----------------------------------------------------------------------*/
 
-app.displayMedia = function(medias, $location, getHtml /* <- callback*/) {
+app.displayMedia = function(
+  medias /* what info to display - as an array - what? */,
+  $location /* cached jQuery object for location for inserting - where? */,
+  getHtml /* callback for what it will look like - how? */
+) {
   $location.html(``);
   medias.forEach(item => {
     let htmlToAppend = getHtml(item);
@@ -89,9 +93,12 @@ app.getPopularByType = function(type) {
       language: app.api.lang
     }
   }).then(data => {
-    // app.getRecent(data.results); // for testing
     const shortData = data.results.slice(0, app.popularAmount); // first x amount as specified in settings
-    app.displayMedia(shortData, app.dom.$popular[`${type}`], app.getItemCardHtml);
+    app.displayMedia(
+      shortData,
+      app.dom.$popular[`${type}`],
+      app.getItemCardHtml
+    );
   });
   /* makes an AJAX call to API */
   /* retrieves popular movies/tv */
@@ -101,10 +108,11 @@ app.getPopularByType = function(type) {
 
 app.getRecent = function(list) {
   let amountToTake = app.recentAmount;
-  const shortData = _.takeRightWhile(list, () => amountToTake-- ); // stops taking when amountToTake hits 0 or nothing left to take
+  const shortData = _.takeRightWhile(list, () => amountToTake--); // stops taking when amountToTake hits 0 or nothing left to take
   if (shortData.length) {
     app.displayMedia(shortData.reverse(), app.dom.$recent, app.getItemCardHtml);
   }
+  console.log(shortData);
   /* retrieves the latest most recent added from app.list */
   /* amount retrieved is specifie by app.recentAmount or whatever is available (if short of recentAmount) */
   /* passes the retrieved medias to displayMedia, algon with location of the Recently Added, and the getItemCardHtml function (as the getHtml callback) */
@@ -122,8 +130,10 @@ app.getByKeyword = function(keyword) {
       query: keyword
     }
   }).then(data => {
-    const shortData = data.results.slice(0, app.resultsAmount); // first x amount as specified in settings // first x amount as specified in settings
-    app.displayMedia(shortData, app.dom.$result, app.getItemCardHtml);
+    // console.log(data);
+    app.getRecent(data.results);
+    // const shortData = data.results.slice(0, app.resultsAmount); // first x amount as specified in settings // first x amount as specified in settings
+    // app.displayMedia(shortData, app.dom.$result, app.getItemCardHtml);
   });
   /* receives keyword provided by user */
   /* creates an url from baseUrl and the end point */
@@ -230,7 +240,7 @@ app.Handlers = function() {
 app.init = function() {
   // app.getByKeyword(`marvel`);
   // app.getDetailsById(`68716`, `tv`);
-  // app.getPopularByType(`movie`);
+  app.getPopularByType(`movie`);
   /* calls getPopularByType  for movies */
   /* calls getRecentByType for tv */
   /* calls to set up app.Handlers  */
